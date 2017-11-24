6f3fd03031bc5a80da991cd7f907315139ace6f6
Ethan Smith <ethan@ethanhs.me>
2017-11-23 16:59:01 -0800

    don't be lazy

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from distutils.core import setup
-import os
+
+
 setup(
     name="typedpkg_stubs",
     url="ethanhs.me",
     author="Ethan Smith",
     author_email="ethan@ethanhs.me",
     version="0.1",
-    package_data={"typedpkg_stubs": os.listdir('typedpkg_stubs')},
+    package_data={"typedpkg_stubs": ['py.typed',
+                                     'sample.pyi',
+                                     '__init__.pyi']},
     packages=["typedpkg_stubs"]
 )
